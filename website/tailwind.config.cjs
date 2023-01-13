768b95f13ea77bd163acd7154e5bbd4aba1556be
Mikael Kristiansson <mikael.kristiansson87@gmail.com>
2023-01-13 10:16:09 +0100

    chore: add website

---
+++ website/tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+
+  theme: {
+    extend: {
+      transitionTimingFunction: {
+        md: 'cubic-bezier(0.4, 0.0, 0.2, 1)',
+      },
+    },
+  },
+}
